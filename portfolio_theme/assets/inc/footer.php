ca0ba236e80da59af6a4b52d5e506a2d5069fe09
TatsunoriOchi <t.ochi.6482@gmail.com>
2024-01-22 06:20:46 +0900

    ファイル移行

--- portfolio_theme/assets/inc/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- フッターここから -->
-<footer class="footer">
-</footer>
-
-<!-- js -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/base.js<?php global $version; echo $version; ?>"></script>
